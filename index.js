4dfcb5056571a6a8d5b5bed84298933dd33c4470
Alexandra Davis <alex.davis806@gmail.com>
2022-03-15 09:03:49 -0700

    commit tree 8483e7c8946180e2c52160ef6f79576980f62d9b

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2= 31;
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num4 = 10
+let num3 = 6
+let mod = num4 - num3
+
+
+let max = Math.max(1, 5, 6, 20);
